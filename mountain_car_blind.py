901813fc08c68669b4b13689927b4753aefb58d1
garrettkatz <garrett.katz@gmail.com>
2022-09-28 23:06:16 -0400

    added proper mc blind rollout experiment

---
+++ mountain_car_blind.py
@@ -0,0 +1,93 @@
+from time import perf_counter
+import numpy as np
+import matplotlib.pyplot as pt
+import gym
+
+def blind_search(env, initial_state, num_steps, max_rollouts, walk_stdev = None):
+
+    for r in range(max_rollouts):
+
+        if walk_stdev is None:
+            actions = np.random.uniform(-1, 1, (num_steps, 1)).astype(np.float32)
+        else:
+            actions = np.random.randn(num_steps, 1).astype(np.float32) * walk_stdev
+            actions[0,0] = np.random.uniform(-1, 1)
+            actions = actions.cumsum(axis=0)
+            actions = np.clip(actions, -1, 1)
+
+        env.state = initial_state
+        for t in range(num_steps):
+            state, reward, terminated, _, _ = env.step(actions[t])
+            if terminated:
+                return actions[:t+1], r+1, True
+
+    return None, max_rollouts, False
+
+def main():
+
+    num_reps = 30
+    max_rollouts = 32 * 8 # comparable computational expense to beam * branching
+    num_steps = 200
+    walk_stdev = 0.1
+
+    env = gym.make("MountainCarContinuous-v0").unwrapped
+
+    success = np.empty((num_reps, 2), dtype=bool)
+    success_indicator = np.zeros((num_reps, 2, max_rollouts * num_steps))
+    run_times = np.empty((num_reps, 2))
+    for rep in range(num_reps):
+        print(f"rep {rep} of {num_reps}")
+        init_state, _ = env.reset()
+        for dw, do_walk in enumerate((False, True)):
+            stdev = walk_stdev if do_walk else None
+            start = perf_counter()
+            actions, num_rollouts, success[rep, dw] = blind_search(env, init_state, num_steps, max_rollouts, stdev)
+            run_times[rep, dw] = perf_counter() - start
+            if success[rep, dw]:
+                step_counts = num_rollouts * num_steps + len(actions)
+                success_indicator[rep, dw, step_counts:] = 1
+
+    env.close()
+
+    uni_avg, rw_avg = success_indicator[:,0,:].mean(axis=0), success_indicator[:,1,:].mean(axis=0)
+    uni_std, rw_std = success_indicator[:,0,:].std(axis=0), success_indicator[:,1,:].std(axis=0)
+    xpts = np.arange(len(uni_avg))
+
+    pt.subplot(2,1,1)
+    ax = pt.gca()
+
+    ax.fill_between(xpts, uni_avg-uni_std, uni_avg+uni_std, color='r', alpha=0.2)
+    ax.fill_between(xpts, rw_avg-rw_std, rw_avg+rw_std, color='b', alpha=0.2)
+
+    ax.plot(xpts, uni_avg, color='r', linestyle='-', label="Uniform")
+    ax.plot(xpts, rw_avg, color='b', linestyle='-', label="Random walk")
+    ax.legend(loc='lower right')
+    ax.set_ylabel("Pr(success)")
+    ax.set_xlabel("Num env steps")
+
+    pt.subplot(2,1,2)
+    uni_xpts = np.sort(run_times[success[:,0],0])
+    uni = np.triu(np.ones((num_reps, len(uni_xpts))))
+    rw_xpts = np.sort(run_times[success[:,1],1])
+    rw = np.triu(np.ones((num_reps, len(rw_xpts))))
+
+    uni_avg, rw_avg = uni.mean(axis=0), rw.mean(axis=0)
+    uni_std, rw_std = uni.std(axis=0), rw.std(axis=0)
+
+    ax = pt.gca()
+
+    ax.fill_between(uni_xpts, uni_avg-uni_std, uni_avg+uni_std, color='r', alpha=0.2)
+    ax.fill_between(rw_xpts, rw_avg-rw_std, rw_avg+rw_std, color='b', alpha=0.2)
+
+    ax.plot(uni_xpts, uni_avg, color='r', linestyle='-', label="Uniform")
+    ax.plot(rw_xpts, rw_avg, color='b', linestyle='-', marker='.', label="Random walk")
+    ax.legend(loc='lower right')
+    ax.set_ylabel("Pr(success)")
+    ax.set_xlabel("Time (s)")
+    
+    pt.tight_layout()
+    pt.show()
+
+
+if __name__ == "__main__": main()
+
--- mountain_car_rollouts.py
+++ mountain_car_rollouts.py
@@ -3,6 +3,26 @@ import numpy as np
 import matplotlib.pyplot as pt
 import gym
 
+def blind_search(env, num_steps, max_rollouts, walk_stdev = None):
+
+    for r in range(max_rollouts):
+
+        if walk_stdev is None:
+            actions = np.random.uniform(-1, 1, (num_steps, 1)).astype(np.float32)
+        else:
+            actions = np.random.randn(num_steps, 1).astype(np.float32) * walk_stdev
+            actions[0,0] = np.random.uniform(-1, 1).astype(np.float32)
+            actions = actions.cumsum(axis=1)
+            actions = np.clip(actions, -1, 1)
+
+        state, _ = env.reset()
+        for t in range(num_steps):
+            state, reward, terminated, _, _ = env.step(actions[t])
+            if success:
+                return actions[:t+1], True
+
+    return None, False
+
 def run(env, actions):
 
     num_rollouts, num_steps = actions.shape[:2]
@@ -10,12 +30,16 @@ def run(env, actions):
     states = np.empty((num_rollouts, num_steps+1, 2), dtype=np.float32)
     rewards = np.empty((num_rollouts, num_steps)).astype(np.float32)
 
+    success = None
     for r in range(num_rollouts):
         states[r, 0], _ = env.reset()
         for t in range(num_steps):
-            states[r, t+1], rewards[r, t], _, _, _ = env.step(actions[r, t])
+            states[r, t+1], rewards[r, t], terminated, _, _ = env.step(actions[r, t])
+            if terminated:
+                success = (r, t)
 
-    return states, rewards
+    return states, rewards, success
+
 
 def main():
 
@@ -34,11 +58,13 @@ def main():
     walk_actions = np.clip(walk_actions, -1, 1)
 
     env = gym.make("MountainCarContinuous-v0")
-    expert_states, expert_rewards = run(env, expert_actions)
-    uniform_states, uniform_rewards = run(env, uniform_actions)
-    walk_states, walk_rewards = run(env, walk_actions)
+    expert_states, expert_rewards, expert_success = run(env, expert_actions)
+    uniform_states, uniform_rewards, uniform_success = run(env, uniform_actions)
+    walk_states, walk_rewards, walk_success = run(env, walk_actions)
     env.close()
 
+    print(f"expert success at {expert_success}")
+    print(f"walk success at {walk_success}")
     print(f"total steps = {num_steps}*{num_rollouts} = {num_steps*num_rollouts}")
 
     for r in range(num_rollouts):
